240a5132778fbb8e4e4c2371638ad1074f4fdf3c
AKASH-SAMPATH <ss.akash001@gmail.com>
2025-02-25 19:33:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,42 +11,48 @@ pageTurnBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     const targetPage = document.getElementById(btn.getAttribute("data-page"));
     
-    if (targetPage.classList.contains("turn")) {
-      targetPage.classList.remove("turn");
-      setTimeout(() => {
-        targetPage.style.zIndex = 10 - Array.from(pages).indexOf(targetPage);
-      }, 500);
-    } else {
-      targetPage.classList.add("turn");
-      setTimeout(() => {
-        targetPage.style.zIndex = 10 + Array.from(pages).indexOf(targetPage);
-      }, 500);
+    if (targetPage) {
+      if (targetPage.classList.contains("turn")) {
+        targetPage.classList.remove("turn");
+        setTimeout(() => {
+          targetPage.style.zIndex = 10 - Array.from(pages).indexOf(targetPage);
+        }, 500);
+      } else {
+        targetPage.classList.add("turn");
+        setTimeout(() => {
+          targetPage.style.zIndex = 10 + Array.from(pages).indexOf(targetPage);
+        }, 500);
+      }
     }
   });
 });
 
 // Contact Me button function
 const contactMeBtn = document.querySelector(".btn.contact-me");
-contactMeBtn.onclick = () => {
-  pages.forEach((page, index) => {
-    setTimeout(() => {
-      page.classList.add("turn");
+if (contactMeBtn) {
+  contactMeBtn.addEventListener("click", () => {
+    pages.forEach((page, index) => {
       setTimeout(() => {
-        page.style.zIndex = 20 + index;
-      }, 500);
-    }, (index + 1) * 200 + 100);
+        page.classList.add("turn");
+        setTimeout(() => {
+          page.style.zIndex = 20 + index;
+        }, 500);
+      }, (index + 1) * 200 + 100);
+    });
   });
-};
+}
 
 // Profile back button
 const backProfileBtn = document.querySelector(".back-profile");
-backProfileBtn.onclick = () => {
-  pages.forEach((_, index) => {
-    setTimeout(() => {
-      pages[index].classList.remove("turn");
+if (backProfileBtn) {
+  backProfileBtn.addEventListener("click", () => {
+    pages.forEach((_, index) => {
       setTimeout(() => {
-        pages[index].style.zIndex = 10 + index;
-      }, 500);
-    }, (index + 1) * 200 + 100);
+        pages[index].classList.remove("turn");
+        setTimeout(() => {
+          pages[index].style.zIndex = 10 + index;
+        }, 500);
+      }, (index + 1) * 200 + 100);
+    });
   });
-};
+}
