4263fe5ba7ac42d6b0461ab532193b3fc1c62f62
AKASH-SAMPATH <ss.akash001@gmail.com>
2025-02-25 18:57:39 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const pageTurnBtn = document.querySelectorAll(".nextprev-btn");
+pageTurnBtn.forEach((el, index) => {
+  el.onclick = () => {
+    const pageTurnId = el.getAttribute("data-page");
+    const pageTurn = document.getElementById(pageTurnId);
+    if (pageTurn.classList.contains("turn")) {
+      pageTurn.classList.remove("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    } else {
+      pageTurn.classList.add("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  };
+});
+
+// Contact Me button function
+const pages = document.querySelectorAll(".book-page.page-right");
+const contactMeBtn = document.querySelector(".btn.contact-me");
+contactMeBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add("turn");
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+// Profile back button
+const backProfileBtn = document.querySelector(".back-profile");
+backProfileBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      pages[index].classList.remove("turn");
+      setTimeout(() => {
+        pages[index].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
